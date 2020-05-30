ba5474f91c0ae48c46d144d9cb93c7ccf932d21a
adolphusnolan <adolphusnolan@gmail.com>
2020-05-30 11:24:11 -0500

    /category

--- cartridge/models/classes/class-marketplace.php
+++ cartridge/models/classes/class-marketplace.php
@@ -972,7 +972,7 @@
 
             $id = $request['id'];
             $domain = $request['domain'];
-            $column = prefixed($domain) . '_id';
+            $column = $domain . '_id';
             $sql = 'DELETE FROM ' . $domain . ' WHERE '.$column.' = :id';
             //echo $id; //exit
             //echo $column; //exit;
